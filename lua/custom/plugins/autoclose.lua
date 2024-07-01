e2318efabee56ae0080a13fd936d9179dc37520f
angelcerveraroldan <48255007+angelcerveraroldan@users.noreply.github.com>
2024-07-01 12:53:42 +0100

    close brackets

---
+++ lua/custom/plugins/autoclose.lua
@@ -0,0 +1,11 @@
+return {
+  'echasnovski/mini.pairs',
+  opts = {
+    mappings = {
+      -- restrict ' with < and & for rust
+      ["'"] = { neigh_pattern = '[^%a\\|<|&].' },
+      -- Add | for rust iterations
+      ['|'] = { action = 'closeopen', pair = '||', neigh_pattern = '[(][)]', register = { cr = false } },
+    },
+  },
+}
